b98a1e6e44884105cd63f7a90e886789ae8c6909
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-07-02 08:48:52 +0200

    regions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import Numbers from "./Components/Numbers";
+import Regions from "./Components/Regions";
 import Services from "./Components/Services";
 
 export default function App() {
@@ -19,73 +20,7 @@ export default function App() {
         <Numbers />
         <Services />
       </div>
-      <div className="flex flex-col justify-center mt-0 w-full text-7xl text-white max-md:max-w-full max-md:text-4xl">
-        <div className="flex flex-col justify-center w-full max-md:max-w-full max-md:text-4xl">
-          <div className="flex flex-col justify-center w-full max-md:max-w-full max-md:text-4xl">
-            <div className="flex overflow-hidden relative flex-col justify-center items-center px-16 py-20 min-h-[1540px] max-md:px-5 max-md:max-w-full max-md:text-4xl">
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/59cc9fa53dc37f25970cbce6d84eb046bcdf047b2f10f1b28186ca7ff60892d6?apiKey=b5c0742263224b4db0641fab4d5d8268&"
-                className="object-cover absolute inset-0 size-full"
-              />
-              <div className="flex relative flex-col items-start mt-96 mb-14 w-full max-w-[1512px] max-md:my-10 max-md:max-w-full max-md:text-4xl">
-                <div className="font-semibold text-blue-300 leading-[114%] max-md:max-w-full max-md:text-4xl">
-                  8 Regions
-                </div>
-                <div className="mt-5 font-light leading-[80px] w-[700px] max-md:max-w-full max-md:text-4xl max-md:leading-[51px]">
-                  of the kingdom fall under our coverage
-                </div>
-                <div className="mt-8 text-2xl leading-8 w-[700px] max-md:max-w-full">
-                  Lorem ipsum dolor sit amet consectetur. Viverra justo odio
-                  platea parturient phasellus aliquam. Gravida amet leo quis nam
-                  nibh.
-                </div>
-                <div className="flex gap-5 justify-between self-stretch px-px mt-11 text-3xl leading-10 text-white max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
-                  <div className="flex flex-col flex-wrap content-center self-start pr-5 mt-7">
-                    <div className="flex gap-5 justify-between whitespace-nowrap">
-                      <div className="justify-center px-5 py-4 rounded-lg bg-white bg-opacity-10 max-md:px-5">
-                        Riyadh
-                      </div>
-                      <div className="justify-center px-5 py-4 rounded-lg bg-white bg-opacity-10 max-md:px-5">
-                        Jeddah
-                      </div>
-                    </div>
-                    <div className="flex gap-5 justify-between mt-6 whitespace-nowrap">
-                      <div className="justify-center px-5 py-4 rounded-lg bg-white bg-opacity-10 max-md:px-5">
-                        Dammam
-                      </div>
-                      <div className="justify-center px-5 py-4 rounded-lg bg-white bg-opacity-10 max-md:px-5">
-                        Madinah
-                      </div>
-                    </div>
-                    <div className="flex gap-5 justify-between mt-6">
-                      <div className="justify-center px-5 py-4 rounded-lg bg-white bg-opacity-10 max-md:px-5">
-                        Al Qassem
-                      </div>
-                      <div className="justify-center px-5 py-4 whitespace-nowrap rounded-lg bg-white bg-opacity-10 max-md:px-5">
-                        Sakaka
-                      </div>
-                    </div>
-                    <div className="flex gap-5 justify-between mt-6">
-                      <div className="justify-center px-5 py-4 rounded-lg bg-white bg-opacity-10 max-md:px-5">
-                        Khamis Mushiat
-                      </div>
-                      <div className="justify-center px-5 py-4 whitespace-nowrap rounded-lg bg-white bg-opacity-10 max-md:px-5">
-                        Tabuk
-                      </div>
-                    </div>
-                  </div>
-                  <img
-                    loading="lazy"
-                    srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/3f9847c31cfd4848a3e0373363032b8bdc7e39ad92d3b1132caca2cc0cec008c?apiKey=b5c0742263224b4db0641fab4d5d8268&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/3f9847c31cfd4848a3e0373363032b8bdc7e39ad92d3b1132caca2cc0cec008c?apiKey=b5c0742263224b4db0641fab4d5d8268&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3f9847c31cfd4848a3e0373363032b8bdc7e39ad92d3b1132caca2cc0cec008c?apiKey=b5c0742263224b4db0641fab4d5d8268&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/3f9847c31cfd4848a3e0373363032b8bdc7e39ad92d3b1132caca2cc0cec008c?apiKey=b5c0742263224b4db0641fab4d5d8268&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/3f9847c31cfd4848a3e0373363032b8bdc7e39ad92d3b1132caca2cc0cec008c?apiKey=b5c0742263224b4db0641fab4d5d8268&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/3f9847c31cfd4848a3e0373363032b8bdc7e39ad92d3b1132caca2cc0cec008c?apiKey=b5c0742263224b4db0641fab4d5d8268&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/3f9847c31cfd4848a3e0373363032b8bdc7e39ad92d3b1132caca2cc0cec008c?apiKey=b5c0742263224b4db0641fab4d5d8268&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/3f9847c31cfd4848a3e0373363032b8bdc7e39ad92d3b1132caca2cc0cec008c?apiKey=b5c0742263224b4db0641fab4d5d8268&"
-                    className="w-full aspect-[1.15] max-md:max-w-full"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Regions />
       <div className="flex gap-5 justify-between self-center mt-52 w-full max-w-[1777px] text-stone-950 max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
         <div className="flex flex-col px-5 max-md:max-w-full">
           <div className="text-7xl font-light leading-[79.8px] max-md:max-w-full max-md:text-4xl">
