65e43bb2157ccd08fcce528105fc4116ad695df9
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-07-02 08:43:44 +0200

    services added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import Numbers from "./Components/Numbers";
+import Services from "./Components/Services";
 
 export default function App() {
   return (
@@ -16,61 +17,7 @@ export default function App() {
       </div>
       <div className="flex z-10 flex-col pl-16 mt-0 w-full max-md:pl-5 max-md:max-w-full">
         <Numbers />
-        <div className="flex gap-5 justify-between mt-52 w-full text-stone-950 max-md:flex-wrap max-md:pr-5 max-md:mt-10 max-md:max-w-full">
-          <div className="flex flex-col max-md:max-w-full">
-            <div className="text-7xl font-light leading-[79.8px] max-md:max-w-full max-md:text-4xl">
-              Duroub Services
-            </div>
-            <div className="mt-8 text-2xl leading-8 max-md:max-w-full">
-              Lorem ipsum dolor sit amet consectetur. Viverra justo odio platea
-              parturient phasellus aliquam. Gravida amet leo quis nam nibh.
-            </div>
-          </div>
-          <div className="flex gap-2.5 self-end mt-36 text-2xl font-semibold capitalize max-md:mt-10">
-            <div className="my-auto">All services</div>
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/7c6be262540d3ceaae66e3e6ca70673fd10251d0f7929c2c89187bf06399bad1?apiKey=b5c0742263224b4db0641fab4d5d8268&"
-              className="shrink-0 border border-blue-900 border-solid aspect-square stroke-[1px] stroke-blue-900 w-[35px]"
-            />
-          </div>
-        </div>
-        <div className="flex overflow-x-auto gap-5 justify-between mt-24 max-md:flex-wrap max-md:mt-10">
-          <div className="flex gap-1.5 max-md:flex-wrap max-md:max-w-full">
-            <div className="grow px-10 pt-96 pb-10 text-4xl text-white rounded-2xl bg-stone-950 leading-[50px] w-fit max-md:px-5 max-md:pt-10 max-md:max-w-full">
-              Transportation, distribution <span className="">&</span> last mile
-            </div>
-            <div className="flex z-10 flex-col justify-center items-start self-end px-9 py-5 mt-10 border-2 border-blue-300 border-solid stroke-[2.371px] stroke-blue-300 max-md:px-5 max-md:mt-10">
-              <div className="shrink-0 bg-blue-300 rounded-3xl h-[43px]" />
-            </div>
-          </div>
-          <div className="flex gap-1.5 max-md:flex-wrap max-md:max-w-full">
-            <div className="grow px-10 pt-96 pb-10 text-4xl text-white rounded-2xl bg-stone-950 leading-[50px] w-fit max-md:px-5 max-md:pt-10 max-md:max-w-full">
-              Warehousing multi temperature <span className="">(</span>25C to
-              ...
-            </div>
-            <div className="flex z-10 flex-col justify-center self-end px-8 py-5 mt-10 border-2 border-blue-300 border-solid stroke-[2.371px] stroke-blue-300 max-md:px-5 max-md:mt-10">
-              <div className="shrink-0 bg-blue-300 rounded-3xl h-[43px]" />
-            </div>
-          </div>
-          <div className="flex gap-1.5 max-md:flex-wrap max-md:max-w-full">
-            <div className="grow px-10 pt-96 pb-10 text-4xl text-white rounded-2xl bg-stone-950 leading-[50px] w-fit max-md:px-5 max-md:pt-10 max-md:max-w-full">
-              Cross border transport
-            </div>
-            <div className="flex z-10 flex-col justify-center items-start self-end px-9 py-5 mt-10 border-2 border-blue-300 border-solid stroke-[2.371px] stroke-blue-300 max-md:px-5 max-md:mt-10">
-              <div className="shrink-0 bg-blue-300 rounded-3xl h-[43px]" />
-            </div>
-          </div>
-          <div className="flex z-10 gap-1.5 max-md:flex-wrap max-md:max-w-full">
-            <div className="grow px-10 pt-96 pb-10 text-4xl text-white rounded-2xl bg-stone-950 leading-[50px] w-fit max-md:px-5 max-md:pt-10 max-md:max-w-full">
-              <span className="">4</span>PL <span className="">&</span> Control
-              tower
-            </div>
-            <div className="flex z-10 flex-col justify-center self-end px-8 py-5 mt-10 border-2 border-blue-300 border-solid stroke-[2.371px] stroke-blue-300 max-md:px-5 max-md:mt-10">
-              <div className="shrink-0 bg-blue-300 rounded-3xl h-[43px]" />
-            </div>
-          </div>
-        </div>
+        <Services />
       </div>
       <div className="flex flex-col justify-center mt-0 w-full text-7xl text-white max-md:max-w-full max-md:text-4xl">
         <div className="flex flex-col justify-center w-full max-md:max-w-full max-md:text-4xl">
